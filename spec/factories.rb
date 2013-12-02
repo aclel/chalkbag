63c1c6a39e4fecf7488040e620a7e990e66ad708
iamwinrar <andrew.cleland3@gmail.com>
2013-12-02 18:50:29 +1000

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Andrew Cleland"
+    email    "andrew.cleland3@gmail.com"
+    password "hehehehe"
+    password_confirmation "hehehehe"
+  end
+end
