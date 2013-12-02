8d7e1d685f84b24c1a9d6428bedbc5c10d324250
iamwinrar <andrew.cleland3@gmail.com>
2013-12-02 16:41:33 +1000

    Finish layouts and routes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 		if page_title.empty?
 			base_title
 		else
-			"#{nase_title} | #{page_title}"
+			"#{base_title} | #{page_title}"
 		end
 	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Chalkbag'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Chalkbag')
-    end
-
-    it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Chalkbag")
-    end
-
-    it "should not have a custom page title" do
-    	visit '/static_pages/home'
-    	expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Chalkbag') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
-	describe "Help page" do
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+  describe "Help page" do
+    before { visit help_path }
 
-		it "should have the base title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("Chalkbag")
-		end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
 
-		it "should not have a custom page title" do
-			visit '/static_pages/help'
-			expect(page).not_to have_title('| Help')
-		end
-	end
+  describe "About page" do
+    before { visit about_path }
 
-	describe "About page" do
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
 
-		it "should have the base title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("Chalkbag")
-		end
+  describe "Contact page" do
+    before { visit contact_path }
 
-		it "should not have a custom page title" do
-			visit '/static_pages/about'
-			expect(page).not_to have_title('| About Us')
-		end
-	end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
